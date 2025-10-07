5705227a729c83111d6edd24257460baba3ba78c
milesmood <53514820+milesmood@users.noreply.github.com>
2025-10-07 20:34:12 +0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "PR Number - $pull_request_number"
 echo "GITHUB_EVENT_PATH - $GITHUB_EVENT_PATH"
 
 giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
-echo "Giphy Response - $giphy_response
+echo "Giphy Response - $giphy_response"
 
 gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
 echo "gif_url - $gif_url"
